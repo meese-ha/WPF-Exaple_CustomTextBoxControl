9b61ad4aa2fe168ddb2eaad84e05e6b45797b059
tpaks <tpaks@DESKTOP-1NL0OLQ>
2024-07-15 18:11:46 +0900

    초기 커밋.

---
+++ WPF Exaple_CustomTextBoxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Exaple_CustomTextBoxControl.View.UserControls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value; 
+                //do not this
+                tbPlaceHolder.Text = placeholder;
+                //OnPropertyChanhed
+
+            }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceHolder.Visibility = Visibility.Hidden;
+            else 
+                tbPlaceHolder.Visibility = Visibility.Visible;
+        
+        }
+    }
+}
